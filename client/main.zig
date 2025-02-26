1528be94bf7a603e4f0cdebe35f2a2c085f57bec
voidKandy <voidkandy@gmail.com>
2025-02-26 15:05:51 -0800

    moved client handshake to root

--- client/main.zig
+++ client/main.zig
@@ -32,8 +32,8 @@ pub fn main() !void {
     defer stream.close();
     print("Connecting to {}\n", .{peer});
 
-    const handshake_cfg = Handshake.Config{ .key = "dGhlIHNhbXBsZSBub25jZQ==", .endpoint = "/chat", .host = "127.0.0.1", .origin = "http://example.com" };
-    var handshake = try Handshake.init(handshake_cfg, allocator);
+    const handshake_cfg = websockets.ClientHandshake.Config{ .key = "dGhlIHNhbXBsZSBub25jZQ==", .endpoint = "/chat", .host = "127.0.0.1", .origin = null };
+    var handshake = try websockets.ClientHandshake.init(handshake_cfg, allocator);
     defer handshake.deinit();
     const body =
         try handshake.body();
@@ -43,68 +43,4 @@ pub fn main() !void {
     print("Sending '{s}' to peer, total written: {d} bytes\n", .{ body.items, size });
     // Or just using `writer.writeAll`
     // try writer.writeAll("hello zig");
-}
-
-const Handshake = struct {
-    const Config = struct {
-        key: []const u8,
-        endpoint: []const u8,
-        host: []const u8,
-        origin: []const u8,
-    };
-    const HeaderMap =
-        std.StringHashMap([]const u8);
-    headers: HeaderMap,
-    endpoint: []const u8,
-    allocator: std.mem.Allocator,
-    const Self = @This();
-
-    fn init(config: Self.Config, allocator: std.mem.Allocator) !Self {
-        var headers = Self.HeaderMap.init(allocator);
-        try headers.put("Host", config.host);
-        try headers.put("Origin", config.origin);
-        try headers.put("Sec-WebSocket-Key", config.key);
-        try headers.put("Upgrade", "websocket");
-        try headers.put("Connection", "Upgrade");
-        // this might need to be configurable
-        try headers.put("Sec-WebSocket-Version", "13");
-        try headers.put("Sec-WebSocket-Protocol", "chat, superchat");
-        return Handshake{
-            .headers = headers,
-            .endpoint = config.endpoint,
-            .allocator = allocator,
-        };
-    }
-
-    fn deinit(self: *Self) void {
-        self.headers.deinit();
-        return;
-    }
-
-    /// converts object into a request body that can be sent
-    fn body(self: *Self) !std.ArrayList(u8) {
-        var buffer = std.ArrayList(u8).init(self.allocator);
-        try buffer.appendSlice("GET ");
-        try buffer.appendSlice(self.endpoint);
-        try buffer.appendSlice(" HTTP/1.1 ");
-        var headers_iter =
-            self.headers.iterator();
-        while (headers_iter.next()) |entry| {
-            try buffer.appendSlice(entry.key_ptr.*);
-            try buffer.appendSlice(": ");
-            try buffer.appendSlice(entry.value_ptr.*);
-            try buffer.append(' ');
-        }
-        try buffer.appendSlice("\r\n");
-        return buffer;
-    }
-};
-
-test "header building" {
-    const allocator = std.testing.allocator;
-    var handshake = try Handshake.init("/chat", "dGhlIHNhbXBsZSBub25jZQ==", "127.0.0.1", "http://www.example.com", allocator);
-    defer handshake.deinit();
-    const body = try handshake.body();
-    defer body.deinit();
-    print("BODY: {s}\n", .{body.items});
 }
--- src/root.zig
+++ src/root.zig
@@ -37,3 +37,74 @@ pub fn connection_information(arg: []const u8) ConnectionInfo {
     return .{ .host = host, .port = port };
     // return try std.net.Address.parseIp4(host, port);
 }
+
+pub const ClientHandshake = struct {
+    pub const Config = struct {
+        /// Base-64 encoded string that the server concatenates with a Globally Unique Identifier
+        /// This concatenated string is then Sha-1 hashed, Base-64 encoded and returned to the client
+        key: []const u8,
+        endpoint: []const u8,
+        host: []const u8,
+        /// Used to protect against unauthorized cross-origin use of a WebSocket server by scripts using the WebSocket API in a web browser.
+        /// This header field is sent by browser clients; for non-browser clients, this header field may be sent if it makes sense in the context of those clients.
+        origin: ?[]const u8,
+    };
+    const HeaderMap =
+        std.StringHashMap([]const u8);
+    headers: HeaderMap,
+    endpoint: []const u8,
+    allocator: std.mem.Allocator,
+    const Self = @This();
+
+    pub fn init(config: Self.Config, allocator: std.mem.Allocator) !Self {
+        var headers = Self.HeaderMap.init(allocator);
+        try headers.put("Host", config.host);
+        if (config.origin) |origin| {
+            try headers.put("Origin", origin);
+        }
+        try headers.put("Sec-WebSocket-Key", config.key);
+        try headers.put("Upgrade", "websocket");
+        try headers.put("Connection", "Upgrade");
+        // this might need to be configurable
+        try headers.put("Sec-WebSocket-Version", "13");
+        try headers.put("Sec-WebSocket-Protocol", "chat, superchat");
+        return Self{
+            .headers = headers,
+            .endpoint = config.endpoint,
+            .allocator = allocator,
+        };
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.headers.deinit();
+        return;
+    }
+
+    /// converts object into a request body that can be sent
+    pub fn body(self: *Self) !std.ArrayList(u8) {
+        var buffer = std.ArrayList(u8).init(self.allocator);
+        try buffer.appendSlice("GET ");
+        try buffer.appendSlice(self.endpoint);
+        try buffer.appendSlice(" HTTP/1.1 ");
+        var headers_iter =
+            self.headers.iterator();
+        while (headers_iter.next()) |entry| {
+            try buffer.appendSlice(entry.key_ptr.*);
+            try buffer.appendSlice(": ");
+            try buffer.appendSlice(entry.value_ptr.*);
+            try buffer.append(' ');
+        }
+        try buffer.appendSlice("\r\n");
+        return buffer;
+    }
+};
+
+test "client handshake building" {
+    const allocator = std.testing.allocator;
+    const handshake_cfg = ClientHandshake.Config{ .key = "dGhlIHNhbXBsZSBub25jZQ==", .endpoint = "/chat", .host = "127.0.0.1", .origin = null };
+    var handshake = try ClientHandshake.init(handshake_cfg, allocator);
+    defer handshake.deinit();
+    const body = try handshake.body();
+    defer body.deinit();
+    std.debug.print("BODY: {s}\n", .{body.items});
+}
